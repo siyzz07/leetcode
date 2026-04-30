ebb1bf28a3e4757eef88efee8318a28597f6c285
shibin siyad <157212839+siyzz07@users.noreply.github.com>
2026-04-30 10:25:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.js
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.js
@@ -2,25 +2,22 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    
-let reverse =0
-let a = Math.abs(x)
-const MAX = 2147483647; 
+var reverse = function (x) {
+
+    let reverse = 0
+    let a = Math.abs(x)
+    const MAX = 2147483647;
     const MIN = -2147483648;
-while(a>0){
+    while (a > 0) {
 
-    let last = a%10
-    reverse = (reverse *10) + last
-    a = Math.floor(a/10)
+        let last = a % 10
+        reverse = (reverse * 10) + last
+        a = Math.floor(a / 10)
 
-}
+    }
 
- let ans = reverse * Math.sign(x)
-if(ans<MAX && ans>MIN){
-    return ans
-}else{
-    return 0
-}
+     
+    return  ans < MAX && ans > MIN ? reverse * Math.sign(x) : 0
+
 
 };
