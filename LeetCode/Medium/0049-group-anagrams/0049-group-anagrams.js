4692b003dd8b028d8009a154c06e074b7b822dc3
shibin siyad <157212839+siyzz07@users.noreply.github.com>
2026-05-02 10:58:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.js
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.js
@@ -5,29 +5,18 @@
 var groupAnagrams = function(strs) {
         let obj = {}
         for(let i = 0 ; i < strs.length ; i++){
-
             let arr = new Array(26).fill(0)
-
             for(let j = 0 ; j< strs[i].length ; j++){
-                
                     let code = strs[i].charCodeAt(j)
                     let val = code-97
                     arr[val]++
-
             }
-
-
             if(obj[arr.join('#')]){
                 obj[arr.join('#')].push(strs[i])
             }else{
                 obj[arr.join('#')] = [strs[i]]
             }
-            
-
         }
-
-        // console.log(obj)
-
         return Object.values(obj)
 
 };
