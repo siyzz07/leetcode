228c2a89dd613a018abe9f740419a52e436b4491
shibin siyad <157212839+siyzz07@users.noreply.github.com>
2026-05-07 10:41:30 +0530

    Time: 5 ms (74.07%), Space: 56.9 MB (83.81%) - LeetHub

---
+++ LeetCode/Hard/0135-candy/0135-candy.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(ratings) {
+  const n = ratings.length;
+    const candies = Array(n).fill(1);
+
+    
+    for (let i = 1; i < n; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    
+    for (let i = n - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        }
+    }
+
+    return candies.reduce((sum, candy) => sum + candy, 0);
+};
